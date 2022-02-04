362658470858f9dc8d50757e1bdd6c94e2e80f16
Bob Green <bob.green@eurofins-digitaltesting.com>
2022-02-04 15:39:04 +0000

    Updated ISR to handle varying brightness on both phases.

--- xmas-lights.ino
+++ xmas-lights.ino
@@ -1,7 +1,5 @@
 /*
- *  This sketch demonstrates how to scan WiFi networks.
- *  The API is almost the same as with the WiFi Shield library,
- *  the most obvious difference being the different file you need to include:
+ *
  */
 #include "WiFi.h"
 
@@ -12,11 +10,18 @@
 #define POSITIVE_PHASE_PIN 26
 #define NEGATIVE_PHASE_PIN 27
 
+#define POSITIVE_POT_PIN 34
+#define NEGATIVE_POT_PIN 35
+
 #define POSITIVE_PHASE 0
 #define NEGATIVE_PHASE 1
 
 volatile int ticks = 0;
 hw_timer_t *timer = NULL;
+
+volatile static int positive_level = 64;
+volatile static int negative_level = 64;
+volatile static int counter;
 
 static int state = POSITIVE_PHASE;
 static int cycles = 0;
@@ -24,20 +29,38 @@ static int cycles = 0;
 void IRAM_ATTR onTimer() {
   ticks++;
 
-  if (ticks == 512) {
+  if (ticks == 256) {
     ticks = 0;
     cycles++;
 
     if (state == POSITIVE_PHASE) {
-      digitalWrite(POSITIVE_PHASE_PIN, 0);
-      digitalWrite(NEGATIVE_PHASE_PIN, 1);
       state = NEGATIVE_PHASE;
+      digitalWrite(POSITIVE_PHASE_PIN, 0);
+      if (negative_level == 0) {
+        digitalWrite(NEGATIVE_PHASE_PIN, 0);
+      }
+      else {
+        digitalWrite(NEGATIVE_PHASE_PIN, 1);
+        counter = negative_level;
+      }
     }
     else {
-      digitalWrite(NEGATIVE_PHASE_PIN, 0);
-      digitalWrite(POSITIVE_PHASE_PIN, 1);
       state = POSITIVE_PHASE;
+      digitalWrite(NEGATIVE_PHASE_PIN, 0);
+      if (positive_level == 0) {
+        digitalWrite(POSITIVE_PHASE_PIN, 0);
+      }
+      else {
+        digitalWrite(POSITIVE_PHASE_PIN, 1);
+        counter = positive_level;
+      }
     }
+  }
+
+  counter--;
+  if (counter <= 0) {
+    digitalWrite(POSITIVE_PHASE_PIN, 0);
+    digitalWrite(NEGATIVE_PHASE_PIN, 0);    
   }
 }
 
@@ -47,26 +70,49 @@ void setup()
 
   Serial.print("Prescaler: ");
   Serial.println(PRESCALER);
-  timer = timerBegin(0, 80, true);
+  timer = timerBegin(0, 160, true);
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmWrite(timer, 10, true);
   timerAlarmEnable(timer);
 
   pinMode(POSITIVE_PHASE_PIN, OUTPUT);
   pinMode(NEGATIVE_PHASE_PIN, OUTPUT);
+  pinMode(POSITIVE_POT_PIN, INPUT);
+  pinMode(NEGATIVE_POT_PIN, INPUT);
+  
   digitalWrite(POSITIVE_PHASE_PIN, 0);
   digitalWrite(NEGATIVE_PHASE_PIN, 0);
 
   Serial.println("Setup done");
+
+  Serial.print("H-Brige control A: Pin ");
+  Serial.println(POSITIVE_PHASE_PIN);
+  
+  Serial.print("H-Brige control B: Pin ");
+  Serial.println(NEGATIVE_PHASE_PIN);
+  
+  Serial.print("Positive channel pot: Pin ");
+  Serial.println(POSITIVE_POT_PIN);
+  
+  Serial.print("Negative channel pot: Pin ");
+  Serial.println(NEGATIVE_POT_PIN);
 }
 
 void loop()
 {
-  register int copyOfTicks;
+  int next_count = 1000;
   
-  Serial.print("Cycles: ");
-  Serial.println(cycles);
+  positive_level = analogRead(POSITIVE_POT_PIN);
+  negative_level = analogRead(NEGATIVE_POT_PIN);
+      
+  Serial.print("Pot A: ");
+  Serial.print(analogRead(POSITIVE_POT_PIN));
 
-  // Wait a bit...
-  delay(2000);
+  Serial.print("  Pot B: ");
+  Serial.print(analogRead(NEGATIVE_POT_PIN));
+
+  Serial.print("  Cycles: ");
+  Serial.print(cycles);
+
+  Serial.print('\r');
 }
