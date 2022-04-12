c72d7aba842044503a7674f32b9d499e21f9f804
Bob Green <bob.green@eurofins-digitaltesting.com>
2022-04-12 19:06:40 +0100

    Added OTA support

--- xmas-lights.ino
+++ xmas-lights.ino
@@ -2,6 +2,9 @@
  *
  */
 #include "WiFi.h"
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #define CLOCK_FREQUENCY 80000000
 #define CYCLES_PER_SECOND 200
@@ -25,6 +28,9 @@ volatile static int counter;
 
 static int state = POSITIVE_PHASE;
 static int cycles = 0;
+
+const char* ssid = "BT-TCCJ6M";
+const char* password = "K69JyKkdNHm7ce";
 
 void IRAM_ATTR onTimer() {
   ticks++;
@@ -68,6 +74,60 @@ void setup()
 {
   Serial.begin(115200);
 
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
   Serial.print("Prescaler: ");
   Serial.println(PRESCALER);
   timer = timerBegin(0, 160, true);
@@ -101,6 +161,8 @@ void setup()
 void loop()
 {
   int next_count = 1000;
+
+  ArduinoOTA.handle();
   
   positive_level = analogRead(POSITIVE_POT_PIN);
   negative_level = analogRead(NEGATIVE_POT_PIN);
